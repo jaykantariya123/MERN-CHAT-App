9de623f79adbe7292e81ef63062d3750caf8679b
jaykantariya123 <113932440+jaykantariya123@users.noreply.github.com>
2023-02-26 19:34:03 +0530

    does: add chat page
    search bar,and profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-
-import './App.css';
-import { Route } from 'react-router-dom';
-import HomePage from './Pages/HomePage';
-import ChatPage from './Pages/ChatPage';
+import "./App.css";
+import Homepage from "./Pages/HomePage";
+import { Route } from "react-router-dom";
+import Chatpage from "./Pages/ChatPage";
 
 function App() {
-  return <div className="App">
-    <Route path="/" component={HomePage} exact />
-    <Route path="/chats" component={ChatPage} />
-  </div>;
+  return (
+    <div className="App">
+      <Route path="/chats" component={Chatpage} />
+      <Route path="/" component={Homepage} exact />
+    </div>
+  );
 }
 
 export default App;
